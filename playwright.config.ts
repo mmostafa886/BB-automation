836f613d1665fbd1686ff34868bbac874d78f102
mmostafa886 <mmostafa886@gmail.com>
2026-07-21 16:45:44 +0300

    SCheduled modification

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     // video: 'retain-on-failure',
-    headless: false,
+    headless: true,
     // headless: !!process.env.CI,
     acceptDownloads: true,
     launchOptions: {
